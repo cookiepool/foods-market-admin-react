617a82ad15b6bc1c8253ddef7e4374dfcfcae2c0
LeeGitHub0817 <18381317534@163.com>
2019-09-11 15:43:12 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import logo from './assets/images/logo.svg';
 import './assets/css/app.css';
 
+
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor(ttt) {
+    super(ttt);
     this.state = {
       name: 'lee'
     }
@@ -40,6 +41,53 @@ class App extends React.Component {
           <p>{ this.state.name }</p>
           <button onClick={this.handleClick}>点我测试</button>
         </div>
+        <hr/>
+        <div>
+          <ChildCom msg={'hello'}></ChildCom>
+        </div>
+      </div>
+    )
+  }
+}
+
+class ChildCom extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props); // {msg: 'hello'}
+    
+    this.users = [
+      { username: 'Jerry', age: 21, gender: 'male' },
+      { username: 'Tomy', age: 22, gender: 'male' },
+      { username: 'Lily', age: 19, gender: 'female' },
+      { username: 'Lucy', age: 20, gender: 'female' }
+    ];
+
+    this.state = {
+      name: 'haha',
+      age: 23
+    };
+    this.handleClickChild = this.handleClickChild.bind(this);
+  }
+  handleClickChild() {
+    this.setState({
+      age: 26
+    });
+  }
+  render() {
+    return (
+      <div>
+        <p>name: {this.state.name}</p>
+        <p>age: {this.state.age}</p>
+        <p>msg: {this.props.msg}</p>
+        <button onClick={this.handleClickChild}>子组件点击测试</button>
+        <hr/>
+        <ul>
+        {
+          this.users.map((item, index) => {
+            return <li onClick={ this.handleClickChild } key={index}>{ item.username }</li>
+          })
+        }
+        </ul>
       </div>
     )
   }
