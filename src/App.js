22cd10df68580856bfabdf6aa3a907a4e6b36aee
LeeGitHub0817 <18381317534@163.com>
2019-09-16 18:36:04 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+// 引入antd
+import { Layout, Icon, Menu } from 'antd';
+const { Header, Footer, Content, Sider} = Layout;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: false, // 是否折叠左边导航栏
+    };
+  };
+
+  toggleNavbar = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  };
+
+  render() {
+    return (
+      <div className="all-wraper">
+        <Layout>
+          {/* 左边导航栏 */}
+          <Sider 
+            width="250px"
+            trigger={null} collapsible
+            collapsed={ this.state.collapsed }
+            style={ { minHeight: '100vh' } }
+          >
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+              <div className="sider-logo"></div>
+              <Menu.Item key="1">
+                <Icon type="dashboard" />
+                <span>数据状态</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="user" />
+                <span>用户管理</span>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Icon type="user" />
+                <span>个人中心</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+
+          {/* 右边主体内容 */}
+          <Layout style={ { minHeight: '100vh' } } >
+            <Header style={{ background: '#fff', paddingLeft: '16px' }}>
+              <Icon
+                className="trigger"
+                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                onClick={this.toggle}
+                style={{fontSize: '26px'}}
+                onClick={ this.toggleNavbar }
+              />
+            </Header>
+            <Content
+              style={{
+                margin: '24px 16px',
+                padding: 24,
+                background: '#fff',
+                minHeight: '1000px',
+              }}
+            >
+              Content
+            </Content>
+            <Footer>Footer
+              dashboard
+              <br></br>
+              dashboard
+              <br></br>
+              dashboard
+              <br></br>
+              dashboard
+              <br></br>
+              dashboard
+              <br></br>
+            </Footer>
+          </Layout>
+        </Layout>
+      </div>
+    )
+  };
+}
+
+export default App;
--- src/examples/example-2/Index.js
+++ src/examples/example-2/Index.js
@@ -4,14 +4,68 @@ class Index extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      log: true,
+      isAgree: false
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleAgree = this.handleAgree.bind(this);
+  }
+
+  handleClick(e) {
+    console.log(this.props);
+    console.log(this.refs.refDom);
+    console.log(this.refs.refTest);
+    console.log(this.state.log);
+    console.log(e);
+  }
+
+  handleAgree() {
+    this.setState({
+      isAgree: true
+    });
   }
 
   render() {
     return (
       <div>
         <h1>欢迎来到首页~~~~~</h1>
+        <hr/>
+        <h1>以下为一些测试</h1>
+        <h2 ref="refDom">ref测试</h2>
+        <TestRef ref="refTest"></TestRef>
+        <button onClick={ this.handleClick /* 这儿不能加(),比如this.handleClick()这样的话页面加载完就会调用函数 */ }>测试ref</button> 
+        <hr/>
+        <h2>测试状态改变</h2>
+        <button onClick={ this.handleAgree }>点击同意</button>
+        {
+          this.state.isAgree === true && <span>已经同意</span>
+        }
+        {
+          this.state.isAgree === false &&  <span>未同意</span>
+        }
+      </div>
+    )
+  }
+}
+
+class TestRef extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lala: 'jej'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log(this.props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>ref测试子组件</p>
+        <button onClick={ this.handleClick }>测试ref-子组件按钮</button>
       </div>
     )
   }
